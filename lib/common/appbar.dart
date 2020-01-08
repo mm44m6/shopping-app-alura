66508f32670056fb85940236ad371f824060891e
Maria Luiza Fornagieri <mm44m6@rocketmail.com>
2020-01-08 11:32:08 -0300

    refactor appbar

--- lib/common/appbar.dart
+++ lib/common/appbar.dart
@@ -11,6 +11,9 @@ final bool ehPaginaCarrinho;
 AppBarCustomizada({this.titulo, this.ehPaginaCarrinho}): super();
 
 @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  
+@override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(titulo, style: TextStyle(fontFamily: 'Open Sans', color: Colors.black)),
@@ -22,65 +25,70 @@ AppBarCustomizada({this.titulo, this.ehPaginaCarrinho}): super();
         color: Colors.black, //change your color here
       ),
       actions: <Widget>[
-        this.ehPaginaCarrinho ? Container() : GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, '/carrinho');
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 8.0,
-            ),
-            child: Container(
-              padding: EdgeInsets.only(right: 30, left: 20),
-              height: 40,
-              alignment: Alignment.centerRight,
-              decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(100),
-                topLeft: Radius.circular(100)),
-                color: Colors.white,
-              ),
-              child: 
-                Consumer<CarrinhoModel>(
-                  builder: (context, carrinho, child) {
-                    if (carrinho.tamanhoListaCarrinho > 0) {
-                      return Stack(
-                        alignment: Alignment.center,
-                          children: <Widget>[
-                            Image(
-                              image: AssetImage('utils/assets/icons/shopping_bag.png'),
-                              height: 30,
-                              fit: BoxFit.fitHeight,
-                            ), 
-                            Positioned(
-                              right: 0,
-                              bottom: 0,
-                              child: new Container(
-                                width: 10,
-                                height: 10,
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(178, 155, 178, 1),
-                                  shape: BoxShape.circle
-                                ),
-                              )
-                            )
-                          ]
-                        );
-                      }
-                      return Image(
-                        image: AssetImage('utils/assets/icons/shopping_bag.png'),
-                          height: 30,
-                          fit: BoxFit.fitHeight,
-                      );
-                    }
-                  )
-                ),
-              )
-            ),
-         ],
+        this.ehPaginaCarrinho ? Container() : _construirBotaoCarrinhoAppBar(context)],
       );
     }
 
-    @override
-    Size get preferredSize => Size.fromHeight(kToolbarHeight);
+    _construirBotaoCarrinhoAppBar(contexto) {
+      return GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(contexto, '/carrinho');
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+          ),
+        child: Container(
+          padding: EdgeInsets.only(right: 30, left: 20),
+          height: 40,
+          alignment: Alignment.centerRight,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(100),
+              topLeft: Radius.circular(100)
+            ),
+          ),
+          child: 
+            Consumer<CarrinhoModel>(
+              builder: (context, carrinho, child) {
+                if (carrinho.tamanhoListaCarrinho > 0) {
+                  return Stack(
+                    alignment: Alignment.center,
+                      children: <Widget>[
+                        _construirImagemBotaoCarrinhoAppBar('utils/assets/icons/shopping_bag.png'), 
+                        _construirIndicatorBotaoCarrinhoAppBar()
+                      ]
+                  );
+                }
+                return _construirImagemBotaoCarrinhoAppBar('utils/assets/icons/shopping_bag.png');
+              }
+            )
+          ),
+        )
+      );
+    }
+
+    _construirIndicatorBotaoCarrinhoAppBar() {
+      return Positioned(
+        right: 0,
+        bottom: 0,
+        child: Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(178, 155, 178, 1),
+            shape: BoxShape.circle
+          ),
+        )
+      );
+    }
+    
+    _construirImagemBotaoCarrinhoAppBar(imagem) {
+      return Image(
+        image: AssetImage(imagem),
+        height: 30,
+        fit: BoxFit.fitHeight,
+      );
+    }
   }
