9b7ac7f77280a8c3a54b3decdc00c61254e883f0
Maria Luiza Fornagieri <mm44m6@rocketmail.com>
2020-01-08 10:51:50 -0300

    refactor details

--- lib/paginas/detalhes.dart
+++ lib/paginas/detalhes.dart
@@ -12,7 +12,8 @@ class Detalhes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: 
+      BoxDecoration(
         image: DecorationImage(
           image: AssetImage("utils/assets/images/${movel['foto']}"), fit: BoxFit.cover)
         ),
@@ -27,52 +28,71 @@ class Detalhes extends StatelessWidget {
               height: 212,
               margin: EdgeInsets.all(16),
               child:  
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
-                        child: Text(movel['titulo'], style: TextStyle(fontSize: 20.0, fontFamily: 'Alata', fontWeight: FontWeight.bold, color: Colors.black))
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-                        child: Text(movel['descricao'], style: TextStyle(fontSize: 12.0, fontFamily: 'Open Sans', color: Color.fromRGBO(83, 83, 83, 1))),
-                      ),
-                      Container(
-                        child: 
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text("${movel['preco']} reais", style: TextStyle(fontSize: 20.0, fontFamily: 'Open Sans', fontWeight: FontWeight.bold, color: Colors.black)),
-  
-                              Material(
-                                color: Color.fromRGBO(178, 155, 178, 0.8),
-                                shape: RoundedRectangleBorder(
-                                borderRadius: new BorderRadius.circular(60.0)),
-                                elevation: 5.0,
-                                child: InkWell(
-                                  borderRadius: BorderRadius.circular(100.0),
-                                  onTap: () => Provider.of<CarrinhoModel>(context).adicionarMovel(movel),
-                                  child: Container(
-                                    padding: EdgeInsets.only(top: 16, bottom: 16, left: 20, right: 20),
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Text('COMPRAR', style: TextStyle(color: Colors.white, fontSize: 12),),
-                                ),
-                              ),
-                            ),
-                          ]
-                        ),
-                        margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-                      )
-                    ],
-                  )
-                ) 
+                _construirCardDetalhes(context),
             )
         )
       )
     );
+  }
+
+  _construirCardDetalhes(contexto) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _construirTituloDetalhes(movel['titulo']),
+            _construirDescricaoDetalhes(movel['descricao']),
+            Container(
+              margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    _construirPrecoDetalhes("${movel['preco']} reais"),
+                     _construirBotaoComprarDetalhes(contexto)
+                  ]
+                ),
+            )
+          ],
+      )
+    );
+  }
+
+  _construirTituloDetalhes(titulo) {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
+      child: Text(titulo, style: TextStyle(fontSize: 20.0, fontFamily: 'Alata', fontWeight: FontWeight.bold, color: Colors.black))
+    );
+  }
+
+  _construirDescricaoDetalhes(descricao) {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      child: Text(descricao, style: TextStyle(fontSize: 12.0, fontFamily: 'Open Sans', color: Color.fromRGBO(83, 83, 83, 1))),
+    );
+  }
+
+  _construirPrecoDetalhes(preco) {
+    return Text(preco, style: TextStyle(fontSize: 20.0, fontFamily: 'Open Sans', fontWeight: FontWeight.bold, color: Colors.black));
+  }
+
+  _construirBotaoComprarDetalhes(contexto) {
+    return Material(
+      color: Color.fromRGBO(178, 155, 178, 0.8),
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(60.0)),
+      elevation: 5.0,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(100.0),
+          onTap: () => Provider.of<CarrinhoModel>(contexto).adicionarMovel(movel),
+          child: Container(
+            padding: EdgeInsets.only(top: 16, bottom: 16, left: 20, right: 20),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              child: Text('COMPRAR', style: TextStyle(color: Colors.white, fontSize: 12),),
+            ),
+          ),
+      );
   }
 }
