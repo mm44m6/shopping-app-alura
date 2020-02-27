e19c5259231539d16091b0152eb0a5af38b6c050
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-27 14:08:26 -0300

    ajustando código para exemplo de lista estática

--- lib/widgets/barra_inferior_carrinho.dart
+++ lib/widgets/barra_inferior_carrinho.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import 'package:shopping_app_alura/modelos/carrinho.dart';
 
 class BarraInferiorCarrinho extends StatelessWidget {
+  final int valor;
 
   final formatacaoReais = NumberFormat.currency(locale: 'pt_BR', symbol: "R\$");
+
+  BarraInferiorCarrinho(this.valor);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-          color: Colors.white,
-          height: 60.0,
-          padding: EdgeInsets.only(left: 20, right: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              _construirTextoBarraInferiorCarrinho('Total', context),
-              Consumer<CarrinhoModel>(
-                builder: (context, carrinho, child) {
-                  return _construirTextoBarraInferiorCarrinho(formatacaoReais.format(carrinho.precoTotal), context);
-                }
-              ),
-            ],
-          ),
+      color: Colors.white,
+      height: 60.0,
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          _construirTextoBarraInferiorCarrinho('Total', context),
+          _construirTextoBarraInferiorCarrinho(
+              formatacaoReais.format(valor), context)
+        ],
+//              Consumer<CarrinhoModel>(
+//                builder: (context, carrinho, child) {
+//                  return _construirTextoBarraInferiorCarrinho(formatacaoReais.format(carrinho.precoTotal), context);
+//                }
+//              ),
+      ),
     );
   }
-  
-  Widget _construirTextoBarraInferiorCarrinho(String texto, BuildContext contexto) {
+
+  Widget _construirTextoBarraInferiorCarrinho(
+      String texto, BuildContext contexto) {
     return Text(texto, style: Theme.of(contexto).textTheme.body1);
   }
-
 }
